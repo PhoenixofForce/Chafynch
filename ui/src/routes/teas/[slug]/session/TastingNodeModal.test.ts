ab022a8db8a328d13ab5f8c15f1afccc193c767c
PhoenixofForce <phoenixofforce@gmail.com>
2026-08-08 21:21:34 +0200

    fix test

--- ui/src/routes/teas/[slug]/session/TastingNodeModal.test.ts
+++ ui/src/routes/teas/[slug]/session/TastingNodeModal.test.ts
@@ -4,7 +4,9 @@ import { screen, render } from '@testing-library/svelte';
 import TastingNoteModal from './TastingNoteModal.svelte';
 
 test('first test', async () => {
-	const { component } = render(TastingNoteModal);
+	const { component } = render(TastingNoteModal, {
+		props: { categories: [{ name: 'foo', subCategories: ['bar-1', 'bar-2'] }] }
+	});
 	component.open();
 	expect(await screen.findByTestId('Category-Picker')).toBeVisible();
 });
