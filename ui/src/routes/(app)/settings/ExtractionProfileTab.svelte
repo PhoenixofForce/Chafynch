fd4143b506acc018d97ef632d4f067212107f490
PhoenixofForce <phoenixofforce@gmail.com>
2026-09-12 16:52:59 +0200

    make extraction profiles importable
    closes #20

--- ui/src/routes/(app)/settings/ExtractionProfileTab.svelte
+++ ui/src/routes/(app)/settings/ExtractionProfileTab.svelte
@@ -4,14 +4,31 @@
 	import { Plus } from '@lucide/svelte';
 	import ExtractionProfileDisplay from './ExtractionProfileDisplay.svelte';
 	import { createEditor } from '$lib/crud/editable.svelte';
+	import Input from '$lib/basics/Input.svelte';
+	import { toast } from '$lib/toast/toast.store.svelte';
 
 	let { profiles = $bindable() }: { profiles: ExtractionProfile[] } = $props();
+	let importProfileJson = $state('');
 
 	const editor = createEditor<ExtractionProfile>();
 	function create() {
+		let baseProfile: Omit<ExtractionProfile, 'id'> = { name: '' };
+		try {
+			if (importProfileJson.length) {
+				baseProfile = JSON.parse(importProfileJson);
+				importProfileJson = '';
+			}
+		} catch (_) {
+			toast.warning('Could not parse JSON.');
+			baseProfile = { name: '' };
+		}
+
+		if (!baseProfile.validUrls) baseProfile.validUrls = [];
+		if (!baseProfile.settings) baseProfile.settings = [];
+
 		const draft: ExtractionProfile = {
-			id: -2,
-			name: ''
+			...baseProfile,
+			id: -2
 		};
 		editor.create(draft);
 	}
@@ -19,11 +36,12 @@
 
 <div class="flex flex-col gap-8">
 	{#if !editor.isNew}
+		<Input inputClass="w-full" placeholder="Paste exported JSON" bind:value={importProfileJson} />
 		<Button
 			class="btn btn-dash btn-primary"
 			disabled={editor.editingAny()}
 			icon={Plus}
-			label="Add new Extraction Profile"
+			label={importProfileJson ? 'Import Extraction Profile' : 'Add new Extraction Profile'}
 			onclick={create}
 		/>
 	{:else}
