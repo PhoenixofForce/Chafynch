4dca81e12d25f5ed51370a374e08f0014c65a61c
PhoenixofForce <phoenixofforce@gmail.com>
2026-09-23 21:21:26 +0200

    cleanup docs deploy by swapping default base path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,7 @@ import starlightLinksValidator from 'starlight-links-validator';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://phoenixofforce.dev',
-	base: process.env.ASTRO_BASE ?? '/Chafynch',
+	base: process.env.ASTRO_BASE ?? '/',
 	trailingSlash: 'always',
 	integrations: [
 		starlight({
